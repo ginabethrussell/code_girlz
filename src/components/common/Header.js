14195c0c5b2a8cfe54b50ab1e71a6a6b7cb67394
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-12-25 15:01:44 -0500

    added responsive styles to splash page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,13 +5,11 @@ import './Header.css';
 function Header () {
     return (
         <div className="header-wrapper">
-            <div className="navbar-wrapper">
-                <div className="logo">
-                    <img src={developerLogo} className="App-logo" alt="girl-developer-logo" />
-                    <h2>Code Girlz</h2>
-                </div>
-                <h2>Creating Cool Things with Code!</h2>
+            <div className="logo">
+                <img src={developerLogo} className="App-logo" alt="girl-developer-logo" />
+                <h2>Code Girlz</h2>
             </div>
+            <h2 className='tagline'>Creating Cool Things with Code!</h2>
         </div>
     );
 }
